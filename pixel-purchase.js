86c7a71090d167de5587b39169b497ee3af9758b
Ifrac <38212474+JJDeakins1@users.noreply.github.com>
2023-09-22 15:06:37 +1000

    Delete pixel-purchase.js

--- pixel-purchase.js
+++
@@ -1,5 +0,0 @@
-fbq('track', 'Purchase', {currency: Shopify.checkout.currency, value: Shopify.checkout.total_price});
-
-console.log("Purchase event triggered")
-console.log(Shopify.checkout.total_price)
-
