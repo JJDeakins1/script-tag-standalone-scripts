50089f5a55b289c68ab939d4d4608084b9c12141
Ifrac <38212474+JJDeakins1@users.noreply.github.com>
2023-09-22 15:06:58 +1000

    Create dynamic-pixel-purchase.js

---
+++ dynamic-pixel-purchase.js
@@ -0,0 +1,4 @@
+fbq('track', 'Purchase', {currency: Shopify.checkout.currency, value: Shopify.checkout.total_price});
+
+console.log("Purchase event triggered")
+console.log(Shopify.checkout.total_price)
