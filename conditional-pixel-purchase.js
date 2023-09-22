a9636ac2e3dc2aa51da62fd11956d98f03b78ef3
Ifrac <38212474+JJDeakins1@users.noreply.github.com>
2023-09-22 15:44:39 +1000

    Update conditional-pixel-purchase.js

--- conditional-pixel-purchase.js
+++ conditional-pixel-purchase.js
@@ -3,7 +3,7 @@ var b = "track";
 var c = "Purchase";
 var d = Shopify.checkout;
 
-if (window.location.pathname.includes("orders") === true) {
+if (window.location.pathname.includes("orders") === false) {
   console.log("Purchase event triggered");
   a(b, c, { currency: d.currency, value: d.total_price });
 } else {
