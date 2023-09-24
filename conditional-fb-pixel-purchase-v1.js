0870d9d32db9a18d1f05a36aee01f7e389331b01
Ifrac <38212474+JJDeakins1@users.noreply.github.com>
2023-09-24 14:57:24 +1000

    Create conditional-fb-pixel-purchase-v1.js

---
+++ conditional-fb-pixel-purchase-v1.js
@@ -0,0 +1,59 @@
+var id = "aw-1111111"
+var label = "43623dsaf23654"
+var divider = "/"
+var purchaseInfo = id+divider+label
+
+var paidUser = true
+
+
+if (window.location.pathname.includes("orders") === false && paidUser === false) {
+  console.log("Google Ads - Purchase Event Triggered");
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', purchaseInfo);
+
+
+  gtag('event', 'conversion', {
+      'send_to': purchaseInfo,
+      'value': Shopify.checkout.total_price,
+      'currency': Shopify.checkout.currency,
+      'transaction_id': Shopify.checkout.order_id
+  });
+
+} else {
+  console.log("Page Path contains Orders");
+}
+
+  if (window.location.pathname.includes("orders") === false && paidUser === true) {
+  console.log("Google Ads - [paidUser] Purchase Event Triggered");
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', purchaseInfo, {'allow_enhanced_conversions': true});
+
+
+  gtag('event', 'conversion', {
+      'send_to': purchaseInfo,
+      'value': Shopify.checkout.total_price,
+      'currency': Shopify.checkout.currency,
+      'transaction_id': Shopify.checkout.order_id
+  });
+
+} else {
+  console.log("Page Path contains Orders");
+}
+
+  gtag('set', 'user_data', {
+    "email": Shopify.checkout.email,
+    "phone_number": Shopify.checkout.phone,
+    "address": {
+    "first_name": Shopify.checkout.shipping_address.first_name,
+    "last_name": Shopify.checkout.shipping_address.last_name,
+    "street": Shopify.checkout.shipping_address.address1,
+    "city":Shopify.checkout.shipping_address.city,
+    "region": Shopify.checkout.shipping_address.province,
+    "postal_code": Shopify.checkout.shipping_address.zip,
+    "country": Shopify.checkout.shipping_address.country_code
+    }
+    });
