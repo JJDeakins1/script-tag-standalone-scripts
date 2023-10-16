bc6d8d8be60b61a1e84c216f8fbb3d009deeaa4b
Ifrac <38212474+JJDeakins1@users.noreply.github.com>
2023-10-16 14:02:13 +1100

    Create google-pixel-1.1.js

---
+++ google-pixel-1.1.js
@@ -0,0 +1,20 @@
+var conversionId;
+var shop = window.Shopify.Checkout.apiHost
+var url = "https://shopify-extention--development.gadget.app/googleData?shopifyShop="
+var getURL = url+shop
+
+fetch(getURL)
+  .then(res => res.json())
+  .then(data => {
+    body = data;
+   })
+  .then(() => {
+  conversionId = body.googleAdsDataRecords[0].conversionId
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', conversionId);
+   })
+  
+
